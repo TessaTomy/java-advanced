77c6dbc1abddbd9d2940cc12274aa04e2d3eb853
Tessa Tomy <tessatomy757@gmail.com>
2026-03-12 21:34:31 +0530

    feat: implement undo/redo functionality with stacks

--- micro-pixel-art/pixel-art-javafx/src/PixelController.java
+++ micro-pixel-art/pixel-art-javafx/src/PixelController.java
@@ -13,6 +13,7 @@ import javafx.embed.swing.SwingFXUtils;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.util.Stack;
 
 public class PixelController {
     @FXML private GridPane gridContainer;
@@ -23,6 +24,10 @@ public class PixelController {
 
     private final double CANVAS_SIZE = 600.0;
     private final String CELL_BG = "#001a33";  
+
+    // Undo/Redo stacks
+    private final Stack<Action> undoStack = new Stack<>();
+    private final Stack<Action> redoStack = new Stack<>();
 
     @FXML
     public void initialize() {
@@ -38,6 +43,8 @@ public class PixelController {
                 newScene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
                     if (event.getCode() == KeyCode.C) handleClear();
                     if (event.getCode() == KeyCode.E) eraserBtn.setSelected(!eraserBtn.isSelected());
+                    if (event.isControlDown() && event.getCode() == KeyCode.Z) handleUndo();
+                    if (event.isControlDown() && event.getCode() == KeyCode.Y) handleRedo();
                 });
             }
         });
@@ -49,7 +56,7 @@ public class PixelController {
     private void handleGenerate() {
         gridContainer.getChildren().clear();
         int size = (int) gridSize.getValue();
-        double cellSize = CANVAS_SIZE / size; // fixed math
+        double cellSize = CANVAS_SIZE / size;
 
         gridContainer.setHgap(1);
         gridContainer.setVgap(1);
@@ -69,17 +76,26 @@ public class PixelController {
     }
 
     private void paintCell(Rectangle cell) {
-        if (eraserBtn.isSelected()) {
-            cell.setFill(Color.web(CELL_BG));
-        } else {
-            cell.setFill(colorPicker.getValue());
+        Color oldColor = (Color) cell.getFill();
+        Color newColor = eraserBtn.isSelected() ? Color.web(CELL_BG) : colorPicker.getValue();
+
+        if (!oldColor.equals(newColor)) {
+            cell.setFill(newColor);
+            undoStack.push(new Action(cell, oldColor, newColor));
+            redoStack.clear(); // once you paint, redo history resets
         }
     }
 
     @FXML
     private void handleClear() {
         gridContainer.getChildren().forEach(n -> {
-            if (n instanceof Rectangle) ((Rectangle) n).setFill(Color.web(CELL_BG));
+            if (n instanceof Rectangle) {
+                Rectangle cell = (Rectangle) n;
+                Color oldColor = (Color) cell.getFill();
+                cell.setFill(Color.web(CELL_BG));
+                undoStack.push(new Action(cell, oldColor, Color.web(CELL_BG)));
+                redoStack.clear();
+            }
         });
     }
 
@@ -97,6 +113,37 @@ public class PixelController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @FXML
+    private void handleUndo() {
+        if (!undoStack.isEmpty()) {
+            Action action = undoStack.pop();
+            action.cell.setFill(action.oldColor);
+            redoStack.push(action);
+        }
+    }
+
+    @FXML
+    private void handleRedo() {
+        if (!redoStack.isEmpty()) {
+            Action action = redoStack.pop();
+            action.cell.setFill(action.newColor);
+            undoStack.push(action);
+        }
+    }
+
+    // Helper class to store actions
+    private static class Action {
+        Rectangle cell;
+        Color oldColor;
+        Color newColor;
+
+        Action(Rectangle cell, Color oldColor, Color newColor) {
+            this.cell = cell;
+            this.oldColor = oldColor;
+            this.newColor = newColor;
         }
     }
 }
